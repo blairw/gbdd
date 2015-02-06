7233351f48686cbfc84424fe0ed8e0e7a52629c4
Blair W <blairwang@outlook.com>
2015-02-06 15:38:23 +1100

    minor update

--- controlpanel.logic.js
+++ controlpanel.logic.js
@@ -24,7 +24,7 @@ function loadAllItems() {
 				"<tr><td>" + allItems[i].item_id
 				+ "</td><td>" + allItems[i].item_name
 				+ "</td><td>" + thisItemTypeNames.join(", ")
-				+ "</td><td>" + "<button class='btn btn-sm btn-primary'>EDIT</button>"
+				+ "</td><td>" + "<button class='100percented btn btn-sm btn-primary'>EDIT</button>"
 				+ "</td></tr>"
 			);
 		}
@@ -32,7 +32,7 @@ function loadAllItems() {
 			"<tr><td>" + "<em>New line:</em>"
 			+ "</td><td>" + "<input type='text' class='form-control' placeholder='New item name' />"
 			+ "</td><td>" + "<input type='text' class='form-control' placeholder='New item types' />"
-			+ "</td><td>" + "<button class='btn btn-sm btn-success'>SAVE</button>"
+			+ "</td><td>" + "<button class='100percented btn btn-sm btn-success'>SAVE</button>"
 			+ "</td></tr>"
 		);
 	});
