8b92b594220f811c5524004e16bf73df729a5a1a
Blair W <blairwang@outlook.com>
2015-02-08 21:01:36 +1100

    prevent race conditions

--- controlpanel.logic.js
+++ controlpanel.logic.js
@@ -34,44 +34,45 @@ function loadAllItems() {
 				"<option value='" + allTypes[i].child_id + "'>" + allTypes[i].child_name + "</option>"
 			);
 		}
-	});
-	
-	var allItemsString = $.get("svc-getItems.php", function(data) {
-		allItems = data;
-		$("#statusbar").html(
-			"<span class='fa fa-check-circle'></span>&nbsp;&nbsp;Loaded " + data.length + " items. "
-			+ "<a href='svc-getItems.php' target='_blank'>See API?</a>"
-		);
-		$("#statusbar").css("background-color", "green");
 		
-		// populate table
-		var thisItemTypeNames = [];
-		for (var i = 0; i < allItems.length; i++) {
-			thisItemTypeNames = [];
-			if (allItems[i].types) {
-				for (var j = 0; j < allItems[i].types.length; j++) {
-					thisItemTypeNames.push(allItems[i].types[j].type_name);
+		// nest the items query inside the item-types query to prevent race conditions
+		var allItemsString = $.get("svc-getItems.php", function(data) {
+			allItems = data;
+			$("#statusbar").html(
+				"<span class='fa fa-check-circle'></span>&nbsp;&nbsp;Loaded " + data.length + " items. "
+				+ "<a href='svc-getItems.php' target='_blank'>See API?</a>"
+			);
+			$("#statusbar").css("background-color", "green");
+			
+			// populate table
+			var thisItemTypeNames = [];
+			for (var i = 0; i < allItems.length; i++) {
+				thisItemTypeNames = [];
+				if (allItems[i].types) {
+					for (var j = 0; j < allItems[i].types.length; j++) {
+						thisItemTypeNames.push(allItems[i].types[j].type_name);
+					}
 				}
+				$("#itemsTbody").append(
+					"<tr><td>" + allItems[i].item_id
+					+ "</td><td>" + allItems[i].item_name
+					+ "</td><td>"
+					+ "<button class='100percented btn btn-sm btn-primary' onclick='editModalShow(" + allItems[i].item_id + ")'>EDIT</button>"
+					+ "</td></tr>"
+				);
 			}
+			// insert new item line (row)
 			$("#itemsTbody").append(
-				"<tr><td>" + allItems[i].item_id
-				+ "</td><td>" + allItems[i].item_name
+				"<tr><td>" + "<em>New line:</em>"
+				+ "</td><td>" + "<input type='text' class='form-control' placeholder='New item name' />"
 				+ "</td><td>"
-				+ "<button class='100percented btn btn-sm btn-primary' onclick='editModalShow(" + allItems[i].item_id + ")'>EDIT</button>"
+				+ "<button class='100percented btn btn-sm btn-success' disabled>"
+				+ "<span class='fa fa-upload'></span>&nbsp;&nbsp;SAVE</button>"
 				+ "</td></tr>"
 			);
-		}
-		// insert new item line (row)
-		$("#itemsTbody").append(
-			"<tr><td>" + "<em>New line:</em>"
-			+ "</td><td>" + "<input type='text' class='form-control' placeholder='New item name' />"
-			+ "</td><td>"
-			+ "<button class='100percented btn btn-sm btn-success' disabled>"
-			+ "<span class='fa fa-upload'></span>&nbsp;&nbsp;SAVE</button>"
-			+ "</td></tr>"
-		);
-		
-	}); // that's all that will get executed upon load of data!
+			
+		}); // ALL ITEMS LOADED
+	}); // ALL LOADED
 }
 function editModalShow(item_id) {
 	$("#editModal").modal("show");
